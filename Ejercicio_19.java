4bd2806f474c1f7b2bb86378773a25c2f84fd659
gadiaz4 <65507572+gadiaz4@users.noreply.github.com>
2020-06-19 23:51:27 -0500

    Create Ejercicio_19.java

---
+++ Ejercicio_19.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_19;
+/**
+ *
+ * @author User
+ */
+import java.util.Scanner;
+public class Ejercicio_19 {
+
+   public static void main(String[] args) {
+    Scanner objeto=new Scanner (System.in);
+    System.out.println("\t\t\tUNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE");
+    System.out.println("Nombre: Díaz Vera Gabriel Alexander");
+    System.out.println("Asignatura: Programación");
+    System.out.println("Nrc: 7450");
+    System.out.println("Tema del Paper:");
+    System.out.println("Transferencia de Aprendizaje Mediante Redes Neuronales Convolucionales para el Reconocimiento de Conductores Distraídos\n");
+    System.out.println("\t\t\tCreación de un programa básico orientado a objetos.\n");
+    System.out.println("El caso de falla de chispa cámara de combustión.");
+    System.out.println("Ingrese el objeto 1: ");
+    String objeto1=objeto.nextLine();
+    System.out.println("Ingrese los datos del objeto 1: ");
+    String dato1=objeto.nextLine();
+    String dato2=objeto.nextLine();
+    String dato3=objeto.nextLine();
+    System.out.println("Ingrese el objeto 2: ");
+    String objeto2=objeto.nextLine();
+    System.out.println("Ingrese los datos del objeto 2: ");
+    String dato21=objeto.nextLine();
+    String dato22=objeto.nextLine();
+    String dato23=objeto.nextLine();
+    System.out.println("Ingrese el objeto 3: ");
+    String objeto3=objeto.nextLine();
+    System.out.println("Ingrese los datos del objeto 3: ");
+    String dato31=objeto.nextLine();
+    String dato32=objeto.nextLine();
+    String dato33=objeto.nextLine();
+    String dato34=objeto.nextLine();
+    System.out.println("Ingrese el objeto 4: ");
+    String objeto4=objeto.nextLine();
+    System.out.println("Ingrese los datos del objeto 4: ");
+    String dato41=objeto.nextLine();
+    String dato42=objeto.nextLine();
+    String dato43=objeto.nextLine();
+    System.out.println("\n");
+    System.out.println("La interfaz esta constituido por lo siguientes objetos  ");
+    System.out.println("\n");
+    System.out.println("1. "+objeto1);
+    System.out.println("Los datos de "+objeto1);
+    System.out.println("\t\t- "+dato1);
+    System.out.println("\t\t- "+dato2);
+    System.out.println("\t\t- "+dato3);
+    System.out.println("Las funciones de "+objeto1);
+    System.out.println("\t\tConoder "+dato1);
+    System.out.println("\t\tDeterminar "+dato2);
+    System.out.println("\t\tIdentificar "+dato3);
+    System.out.println("\n");
+    System.out.println("2. "+objeto2);
+    System.out.println("Los datos de "+objeto2);
+    System.out.println("\t\t- "+dato21);
+    System.out.println("\t\t- "+dato22);
+    System.out.println("\t\t- "+dato23);
+    System.out.println("Las funciones de "+objeto2);
+    System.out.println("\t\tIdentificar "+dato21);
+    System.out.println("\t\tDefinir "+dato22);
+    System.out.println("\t\tDetallar "+dato23);
+    System.out.println("\n");
+    System.out.println("3. "+objeto3);
+    System.out.println("Los datos de "+objeto3);
+    System.out.println("\t\t- "+dato31);
+    System.out.println("\t\t- "+dato32);
+    System.out.println("\t\t- "+dato33);
+    System.out.println("\t\t- "+dato34);
+    System.out.println("Las funciones de "+objeto3);
+    System.out.println("\t\tEstablecer "+dato31);
+    System.out.println("\t\tProporcionar "+dato32);
+    System.out.println("\t\tConocer "+dato33);
+    System.out.println("\t\tVerificar "+dato34);
+    System.out.println("\n");
+    System.out.println("4. "+objeto4);
+    System.out.println("Los datos de "+objeto4);
+    System.out.println("\t\t- "+dato41);
+    System.out.println("\t\t- "+dato42);
+    System.out.println("\t\t- "+dato43);
+    System.out.println("Las funciones de "+objeto4);
+    System.out.println("\t\tIdentificar "+dato41);
+    System.out.println("\t\tReconocer "+dato42);
+    System.out.println("\t\tCategorizar "+dato43);
+    }
+
+}
